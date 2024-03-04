c21957e6f9706d159d9fb3cc372d2c795859d059
Gustavo de Assis Costa <gacosta@gmail.com>
2024-03-04 20:49:35 -0300

    Update and rename test_list.py to test_1.py

---
+++ test_1.py
@@ -0,0 +1,4 @@
+import lista
+
+def list_test():
+  assert lista.inverse_list() == [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
--- test_list.py
+++
@@ -1,4 +0,0 @@
-import list
-
-def list_test():
-  assert list.inverse_list() == [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
