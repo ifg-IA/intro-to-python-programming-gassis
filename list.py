5f0c0cfb4845099e3034843a49432e9694d24fcb
Gustavo de Assis Costa <gacosta@gmail.com>
2024-03-04 17:43:25 -0300

    Update list.py

--- list.py
+++ list.py
@@ -1,3 +1,5 @@
-# Gerar uma lista de valores numericos de tamanho 10 e imprimi-la na ordem inversa
-
-print(lista)
+# Gerar uma lista de valores numéricos de tamanho 10 e imprimi-la na ordem inversa
+def inverse_list():
+  lista = []
+  # Especificar seu código aqui.
+  return lista
